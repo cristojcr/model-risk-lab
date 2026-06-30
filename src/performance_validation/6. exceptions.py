97258d5504f7fa14d53062ab1393d0ebdf13d399
cristojcr <cristo.jcr@gmail.com>
2026-06-30 10:19:45 -0300

    Update 6. exceptions.py

--- src/performance_validation/6. exceptions.py
+++ src/performance_validation/6. exceptions.py
@@ -1,1 +1,23 @@
+"""
+Custom exceptions for Performance Validation.
+"""
 
+
+class PerformanceValidationError(Exception):
+    """Base exception for Performance Validation."""
+
+
+class MissingColumnError(PerformanceValidationError):
+    """Required column not found."""
+
+
+class InvalidProbabilityError(PerformanceValidationError):
+    """Predicted probabilities must be between 0 and 1."""
+
+
+class DifferentLengthError(PerformanceValidationError):
+    """Arrays have different lengths."""
+
+
+class MissingValuesError(PerformanceValidationError):
+    """Input data contains missing values."""
